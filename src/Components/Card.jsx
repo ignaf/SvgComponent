2c9c97fe6b1afd491b49259c28bb6acbbabdf34e
igna focas <ignaa88@gmail.com>
2022-10-05 11:24:57 -0300

    estilos card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="card">
-      <div className="card-body">
+      <div className="card-body card--svg">
         <h5 className="card-title text-center">{props.title}</h5>
         <div>{props.children}</div>
       </div>
